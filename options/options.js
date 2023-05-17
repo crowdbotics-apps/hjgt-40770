ffccb4ebb28e75c1a4d1d7dbf692777d37d03224
Bojan Petrusevski <bojan.petrusevski@crowdbotics.com>
2023-05-17 17:01:39 +0200

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "hurricanejunior-gol-40770",
   url: "https://hurricanejunior-gol-40770.botics.co",
   api: "https://hurricanejunior-gol-40770.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
---
+++ screens/Untitled2/index.js
@@ -0,0 +1,39 @@
+import { useNavigation } from "@react-navigation/native";
+import { Pressable } from "react-native";
+import { Text } from "react-native";
+import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
+
+const Untitled2 = () => {
+  const navigation = useNavigation();
+  return <SafeAreaView style={styles.safeArea}>
+        <ScrollView contentContainerStyle={{
+      backgroundColor: '#f0f0f1',
+      padding: 10,
+      position: 'relative',
+      flex: 1
+    }}><Pressable onPress={() => {
+        navigation.navigate("ScreenAI3");
+      }}><Text style={styles.BADZAobv}>{"Hamburger - menu"}</Text></Pressable><Text style={styles.MhqsYLia}>{"Complete your Experience"}</Text></ScrollView>
+    </SafeAreaView>;
+};
+
+const styles = StyleSheet.create({
+  safeArea: {
+    height: '100%'
+  },
+  BADZAobv: {
+    width: 100,
+    height: 50,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0
+  },
+  MhqsYLia: {
+    width: 100,
+    height: 50,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0
+  }
+});
+export default Untitled2;
